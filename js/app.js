ac36cd9ab61d754281c29d6cf9ba0d0314cc0b04
Duy Hoang Hua <huahoangduy.389@gmail.com>
2021-08-12 14:28:39 +0700

    change navigation

--- js/app.js
+++ js/app.js
@@ -19,3 +19,23 @@ $(".overflow").on('click', (e) => {
     $(".header-container").removeClass("active");
     $(".overflow").removeClass("active");
 });
+
+// scrool with href with #
+$(document).on('click', 'a[href^="#"]', function (e) {
+    // target element id
+    var id = $(this).attr('href');
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+    // add class active if user hit
+    $(".nav-item-link").removeClass("active");
+    $(this).addClass("active");
+    // top position relative to the document
+    var pos = $id.offset().top - 120;
+    // animated top scrolling
+    $('body, html').animate({ scrollTop: pos });
+});
